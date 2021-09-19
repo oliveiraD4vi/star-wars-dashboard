dc62b9420f482be9350d0a2f82addbfdcf272805
Davi Oliveira <davi.comp.oliveira@gmail.com>
2021-09-19 17:15:37 -0300

    [005] adicao de tabela e requisiçoes referentes

--- _js/index.js
+++ _js/index.js
@@ -1,14 +1,30 @@
-const planetasContador = document.getElementById('planetas');
+const planetasContador    = document.getElementById('planetas');
 const personagensContador = document.getElementById('personagens');
-const navesContador = document.getElementById('naves');
-const luasContador = document.getElementById('luas');
+const navesContador       = document.getElementById('naves');
+const luasContador        = document.getElementById('luas');
 
 preencheCont();
+preencheTabe();
+
+async function preencheTabe() {
+	const response = await getReq('films/');
+	const tableData = response.data.results;
+
+	tableData.forEach(film => {
+		$('#films-table').append(
+			`<tr>
+				<td>${film.title}</td>
+				<td>${moment(film.release_date).format('DD/MM/YYYY')}</td>
+				<td>${film.director}</td>
+				<td>${film.episode_id}</td>
+			</tr>`
+		);
+	})
+}
 
 function preencheCont() {
 	Promise.all([getReq('planets/'), getReq('people/'), getReq('starships/'), getReq('vehicles/')])
 		.then(function (response) {
-			console.log("==>", response);
 			planetasContador.innerHTML    = response[0].data.count;
 			personagensContador.innerHTML = response[1].data.count;
 			navesContador.innerHTML       = response[2].data.count;
